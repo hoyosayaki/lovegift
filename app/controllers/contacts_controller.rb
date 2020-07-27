147fbf982b4c879812dc1483d8b39db0ddcaa8a2
hoyosayaki <kiya.sayoho@gmail.com>
2020-07-27 23:23:27 +0900

    各ページの叩き作成

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,21 @@ class ContactsController < ApplicationController
     def new
     end
 
+    def create
+        if params[:submit]
+          @contact = Contact.new(contact_params)
+          @contact.save
+          redirect_to new_contact_path
+        end
+      end
+
     def show
     end
+
+    private
+
+    def contact_params
+      params.require(:contact).permit(:name, :birthday, :like, :not_like, :memo)
+    end
+
 end
